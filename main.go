3640b09381a9469a810c9b6cacd76dca2dd2bd86
kazukichi <zexyl_x@icloud.com>
2020-03-28 13:12:36 +0900

    ひとまず簡単にBitcoin表示まで

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+
+	"github.com/labstack/gommon/log"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// 依存パッケージの読み込み > グローバル定数 > グローバル変数 > init() > main() の順に実行（判定）される
+
+// bitflyerのベースurlとエンドポイント
+const bitflyerBaseUrl = "https://api.bitflyer.jp/v1/getticker?product_code="
+
 var e = createMux()
 
 func main() {
+	// パスとarticleIndexを紐付けている
 	e.GET("/", articleIndex)
 
 	e.Logger.Fatal(e.Start(":8080"))
@@ -26,5 +35,31 @@ func createMux() *echo.Echo {
 }
 
 func articleIndex(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.String(http.StatusOK, GetBitcoinApi())
+}
+
+// BitFlyerのBitcoinのAPIを取得する関数
+func GetBitcoinApi() string {
+
+	// GetでWebAPIに対してアクセスする
+	resp, err := http.Get(bitflyerBaseUrl + "btc_jpy")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 最後にapiをCloseする
+	defer resp.Body.Close()
+
+	// ReadAllは、エラーまたはEOFに達するまで読み込み、読み込んだデータを返す
+	byteArray, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// byte配列をstring型へキャスト
+	castString := string(byteArray)
+
+	// a := json.Unmarshal(byteArray)
+
+	return castString
 }
