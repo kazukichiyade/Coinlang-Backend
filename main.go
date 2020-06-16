776f05fb2b2adad928995863b150471fadf72501
kazukichi <zexyl_x@icloud.com>
2020-06-17 00:14:16 +0900

    構造体デコード処理途中

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"time"
-
-	// "encoding/json"
-	// "fmt"
 
 	"github.com/labstack/gommon/log"
 
@@ -21,11 +18,11 @@ const bitflyerBaseURL = "https://api.bitflyer.jp/v1/getticker?product_code="
 
 // BitflyerCoin is Bitflyerにあるコインの構造体 (jsonのkeyの表記を変更できるかつkeyと一致しないvalueは保持しない)
 type BitflyerCoin struct {
-	Coin            string    `json:"product_code"` // コイン名
-	Time            time.Time `json:"timestamp"`    // 時間
-	BestBid         int       `json:"best_bid"`     // 売値
-	BestAsk         int       `json:"best_ask"`     // 買値
-	LastTradedPrice int       `json:"ltp"`          // 最終取引価格
+	Coin            string  `json:"product_code"` // コイン名
+	Time            string  `json:"timestamp"`    // 時間
+	BestBid         float64 `json:"best_bid"`     // 売値
+	BestAsk         float64 `json:"best_ask"`     // 買値
+	LastTradedPrice float64 `json:"ltp"`          // 最終取引価格
 }
 
 var e = echoStart()
@@ -54,13 +51,13 @@ func echoStart() *echo.Echo {
 func articleIndex(c echo.Context) error {
 
 	// ステータスコード 200 で、GetBitcoinAPI関数で取得した文字列をレスポンス
-	return c.String(http.StatusOK, GetBitcoinAPI())
+	return c.JSON(http.StatusOK, GetBitcoinAPI())
 }
 
 // GetBitcoinAPI is BitFlyerのBitcoinのAPIを取得する関数
-func GetBitcoinAPI() string {
+func GetBitcoinAPI() []BitflyerCoin {
 
-	// GetでWebAPIに対してアクセスする
+	// WebAPIに対してアクセスする
 	resp, err := http.Get(bitflyerBaseURL + "btc_jpy")
 	if err != nil {
 		log.Fatal(err)
@@ -69,22 +66,32 @@ func GetBitcoinAPI() string {
 	// 最後にWebAPIに対してのアクセスをCloseする
 	defer resp.Body.Close()
 
-	// ReadAllは、エラーまたはEOFに達するまで読み込み、読み込んだデータを返す
+	// jsonを読み込む
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 対応出来ていない
-	// var bitcoin BitflyerBitcoin
+	var bitcoin []BitflyerCoin
+	if err := json.Unmarshal(body, &bitcoin); err != nil {
+		log.Fatal(err)
+	}
+
+	// var bitcoin BitflyerCoin
 	// if err := json.Unmarshal(body, &bitcoin); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(bitcoin)
+
+	// u := new(BitflyerCoin)
+	// err = json.Unmarshal(body, u)
+	// if err != nil {
 	// 	log.Fatal(err)
 	// }
 	// fmt.Println(body)
 
 	// byte配列をstring型へキャスト
-	BitcoinString := string(body)
+	// BitcoinString := string(bitcoin)
 
-	return BitcoinString
+	return bitcoin
 }
